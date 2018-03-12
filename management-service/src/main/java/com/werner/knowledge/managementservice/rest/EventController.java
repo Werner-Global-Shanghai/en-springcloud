53c9d72fd9f1be9afc5984eb3fd9ce2d5c18531f
Jerry <leeming-jiang@163.com>
2018-03-12 14:02:40 +0800

    add spring cloud gateway

--- management-service/src/main/java/com/werner/knowledge/managementservice/rest/EventController.java
+++ management-service/src/main/java/com/werner/knowledge/managementservice/rest/EventController.java
@@ -3,11 +3,17 @@ package com.werner.knowledge.managementservice.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 @RestController
 public class EventController {
 
     @GetMapping("/getdemo")
-    public String getDemoEventType() {
-        return "Hello_event";
+    public String getDemoEventType() throws UnknownHostException {
+
+        String hostname = Inet4Address.getLocalHost().getCanonicalHostName();
+
+        return hostname + " : Hello_event";
     }
 }
