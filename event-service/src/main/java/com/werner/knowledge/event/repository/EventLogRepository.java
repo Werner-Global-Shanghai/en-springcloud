9f4039fbcb546c7043be3d1c13f9e630ea0567ef
Jerry <leeming-jiang@163.com>
2018-03-16 09:59:24 +0800

    add log api

--- event-service/src/main/java/com/werner/knowledge/event/repository/EventLogRepository.java
+++ event-service/src/main/java/com/werner/knowledge/event/repository/EventLogRepository.java
@@ -2,6 +2,8 @@ package com.werner.knowledge.event.repository;
 
 import com.werner.knowledge.event.model.EventLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventLogRepository extends JpaRepository<EventLog, Long> {
 }
---
+++ event-service/src/main/java/com/werner/knowledge/event/rest/EventLogResource.java
@@ -0,0 +1,25 @@
+package com.werner.knowledge.event.rest;
+
+import com.werner.knowledge.event.model.EventLog;
+import com.werner.knowledge.event.repository.EventLogRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/logs")
+public class EventLogResource {
+
+    private final EventLogRepository eventLogRepository;
+
+    public EventLogResource(EventLogRepository eventLogRepository) {
+        this.eventLogRepository = eventLogRepository;
+    }
+
+    @GetMapping("/all")
+    public List<EventLog> getAllLogs() {
+        return eventLogRepository.findAll();
+    }
+}
