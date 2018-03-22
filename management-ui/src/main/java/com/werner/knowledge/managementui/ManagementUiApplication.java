df7c5a77cb174f359b484ad755db7b4d88799c15
Jerry <leeming-jiang@163.com>
2018-03-22 15:25:05 +0800

    add management ui demo

---
+++ management-ui/src/main/java/com/werner/knowledge/managementui/ManagementUiApplication.java
@@ -0,0 +1,12 @@
+package com.werner.knowledge.managementui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementUiApplication.class, args);
+	}
+}
---
+++ management-ui/src/test/java/com/werner/knowledge/managementui/ManagementUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.werner.knowledge.managementui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagementUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
